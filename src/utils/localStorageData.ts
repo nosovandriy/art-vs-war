392dc9a9ae2ac97f03853e96eafd583fffa72a78
Andriy Nosov <nosovandriy@gmail.com>
2023-08-22 15:56:31 +0300

    bugfix/cart-local-storage fix delete image from cart

--- src/utils/localStorageData.ts
+++ src/utils/localStorageData.ts
@@ -4,7 +4,11 @@ import getTotalPrice from "./calcTotalPrice";
 export const getDataFromLocalStorage = (): DataFromLocalStorage => {
   const data = localStorage.getItem("cart");
   const paintingsFromLocalStorage = data ? JSON.parse(data) : [];
-  const totalPriceFromLocalStorage = getTotalPrice(paintingsFromLocalStorage);
+  let totalPriceFromLocalStorage = 0;
+
+  if (paintingsFromLocalStorage.length > 0) {
+    totalPriceFromLocalStorage = getTotalPrice(paintingsFromLocalStorage);
+  }
 
   return {
     paintingsFromLocalStorage,
@@ -16,4 +20,3 @@ export const setDataToLocalStorage = (paintings: CartItem[]) => {
   const json = JSON.stringify(paintings);
   localStorage.setItem("cart", json);
 };
-
