48e4afa3269d085cf23887747ce26132ec6ba15b
Andriy Nosov <nosovandriy@gmail.com>
2023-09-30 23:48:53 +0300

    feature/contact-page update next

--- next.config.js
+++ next.config.js
@@ -23,14 +23,14 @@ const nextConfig = {
       },
     ],
   },
-  // webpack: (config, { isServer, nextRuntime, webpack }) => {
-  //   // Avoid AWS SDK Node.js require issue
-  //   if (isServer && nextRuntime === "nodejs")
-  //     config.plugins.push(
-  //       new webpack.IgnorePlugin({ resourceRegExp: /^aws-crt$/ })
-  //     );
-  //   return config;
-  // },
+  webpack: (config, { isServer, nextRuntime, webpack }) => {
+    // Avoid AWS SDK Node.js require issue
+    if (isServer && nextRuntime === "nodejs")
+      config.plugins.push(
+        new webpack.IgnorePlugin({ resourceRegExp: /^aws-crt$/ })
+      );
+    return config;
+  },
 };
 
 module.exports = nextConfig;
