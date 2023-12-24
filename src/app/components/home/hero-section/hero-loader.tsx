ee14b24e6378abb1e3c6d75b68c7450cfb5562cf
Andriy Nosov <nosovandriy@gmail.com>
2023-12-24 13:37:16 +0200

    feature/hero-video add 22 examples

--- src/app/components/home/hero-section/hero-loader.tsx
+++ src/app/components/home/hero-section/hero-loader.tsx
@@ -5,16 +5,28 @@ const Hero = async () => {
   const paintings = await getHeroPaintings();
 
   const videos = [
-    { src: '/assets/video1_cropped.mp4', text: 'video1.mp4'  },
-    { src: '/assets/video2_cropped.mp4', text: 'video2.mp4'  },
-    { src: '/assets/video3_cropped.mp4', text: 'video3.mp4'  },
-    { src: '/assets/video4_cropped.mp4', text: 'video4.mp4'  },
-    { src: '/assets/video5_cropped.mp4', text: 'video5.mp4'  },
-    { src: '/assets/video6_cropped.mp4', text: 'video6.mp4'  },
-    { src: '/assets/video7_cropped.mp4', text: 'video7.mp4'  },
-    { src: '/assets/video8_cropped.mp4', text: 'video8.mp4'  },
-    { src: '/assets/video9_cropped.mp4', text: 'video9.mp4'  },
-    { src: '/assets/video10_cropped.mp4', text: 'video10.mp4' },
+    { src: '/assets/video1.mp4', text: 'video1.mp4'  },
+    { src: '/assets/video2.mp4', text: 'video2.mp4'  },
+    { src: '/assets/video3.mp4', text: 'video3.mp4'  },
+    { src: '/assets/video4.mp4', text: 'video4.mp4'  },
+    { src: '/assets/video5.mp4', text: 'video5.mp4'  },
+    { src: '/assets/video6.mp4', text: 'video6.mp4'  },
+    { src: '/assets/video7.mp4', text: 'video7.mp4'  },
+    { src: '/assets/video8.mp4', text: 'video8.mp4'  },
+    { src: '/assets/video9.mp4', text: 'video9.mp4'  },
+    { src: '/assets/video10.mp4', text: 'video10.mp4' },
+    { src: '/assets/video11.mp4', text: 'video11.mp4' },
+    { src: '/assets/video12.mp4', text: 'video12.mp4' },
+    { src: '/assets/video13.mp4', text: 'video13.mp4' },
+    { src: '/assets/video14.mp4', text: 'video14.mp4' },
+    { src: '/assets/video15.mp4', text: 'video15.mp4' },
+    { src: '/assets/video16.mp4', text: 'video16.mp4' },
+    { src: '/assets/video17.mp4', text: 'video17.mp4' },
+    { src: '/assets/video18.mp4', text: 'video18.mp4' },
+    { src: '/assets/video19.mp4', text: 'video19.mp4' },
+    { src: '/assets/video20.mp4', text: 'video20.mp4' },
+    { src: '/assets/video21.mp4', text: 'video21.mp4' },
+    { src: '/assets/video22.mp4', text: 'video22.mp4' },
   ];
 
   const randomIndex = Math.floor(Math.random() * videos.length);
