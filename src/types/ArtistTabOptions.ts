02fddc6a5cbd7459acbd31b4f9bee53a3efa304f
Rafael <rafmamedov@gmail.com>
2024-02-10 13:36:21 +0200

    FIXED: Opening the accordeon tab via search params, added validation pattern in about me fields

--- src/types/ArtistTabOptions.ts
+++ src/types/ArtistTabOptions.ts
@@ -6,3 +6,11 @@ export enum ArtistTabOptions {
   payment = "Payment",
   profile = "Profile",
 }
+
+export enum TabOptionsKeys {
+  "Artworks" = "0",
+  "Collections" = "1",
+  "Art Process" = "2",
+  "Delivery" = "3",
+  "Payment" = "4",
+};
